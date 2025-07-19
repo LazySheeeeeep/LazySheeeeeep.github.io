85bec069cf48ca85a850dbc915baca4d47e9f6ac
YaoYang <140060080+LazySheeeeeep@users.noreply.github.com>
2025-07-19 09:55:36 +0800

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,5 +1,5 @@
 /* ==========================================================================
-   Various functions that we want to use within the template
+   jQuery plugin settings and other scripts
    ========================================================================== */
 
 // detect OS/browser preference
@@ -30,11 +30,7 @@ var toggleTheme = () => {
   setTheme(new_theme);
 };
 
-/* ==========================================================================
-   Actions that should occur when the page has been fully loaded
-   ========================================================================== */
-
-$(document).ready(function () {
+$(document).ready(function(){
   // If the user hasn't chosen a theme, follow the OS preference
   setTheme();
   window.matchMedia('(prefers-color-scheme: dark)')
@@ -47,74 +43,63 @@ $(document).ready(function () {
   // Enable the theme toggle
   $('#theme-toggle').on('click', toggleTheme);
 
-  // Enable the sticky footer
-  var bumpIt = function () {
-    $("body").css("margin-bottom", $(".page__footer").outerHeight(true));
-  }
-  $(window).resize(function () {
+  // Sticky footer
+  var bumpIt = function() {
+      $("body").css("margin-bottom", $(".page__footer").outerHeight(true));
+    },
+    didResize = false;
+
+  bumpIt();
+
+  $(window).resize(function() {
     didResize = true;
   });
-  setInterval(function () {
+  setInterval(function() {
     if (didResize) {
       didResize = false;
       bumpIt();
-    }}, 250);
-  var didResize = false;
-  bumpIt();
-
-  // FitVids init - 只有在 fitvids 函数存在时才调用
-  if (typeof fitvids === 'function') {
-    fitvids();
-  }
+    }
+  }, 250);
+  
+  // FitVids init
+  fitvids();
 
   // Follow menu drop down
-  $(".author__urls-wrapper button").on("click", function () {
-    $(".author__urls").fadeToggle("fast", function () { });
+  $(".author__urls-wrapper button").on("click", function() {
+    $(".author__urls").fadeToggle("fast", function() {});
     $(".author__urls-wrapper button").toggleClass("open");
   });
 
-  // Restore the follow menu if toggled on a window resize
-  jQuery(window).on('resize', function () {
-    if ($('.author__urls.social-icons').css('display') == 'none' && $(window).width() >= 925) {
-      $(".author__urls").css('display', 'block')
-    }
-  });
-
-  // Init smooth scroll
-  $("a").smoothScroll({
-    offset: -70,
-    preventDefault: false,
-  });
+  // init smooth scroll, this needs to be slightly more than then fixed masthead height
+  $("a").smoothScroll({offset: -65});
 
   // add lightbox class to all image links
   $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
 
-  // Magnific-Popup options - 只有在 magnificPopup 存在时才调用
-  if ($.fn.magnificPopup) {
-    $(".image-popup").magnificPopup({
-      type: 'image',
-      tLoading: 'Loading image #%curr%...',
-      gallery: {
-        enabled: true,
-        navigateByImgClick: true,
-        preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-      },
-      image: {
-        tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
-      },
-      removalDelay: 500, // Delay in milliseconds before popup is removed
-      // Class that is added to body when popup is open.
-      // make it unique to apply your CSS animations just to this exact popup
-      mainClass: 'mfp-zoom-in',
-      callbacks: {
-        beforeOpen: function() {
-          // just a hack that adds mfp-anim class to markup
-          this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-        }
-      },
-      closeOnContentClick: true,
-      midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
-    });
-  }
+  // Magnific-Popup options
+  $(".image-popup").magnificPopup({
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
+    },
+    removalDelay: 500, // Delay in milliseconds before popup is removed
+    // Class that is added to body when popup is open.
+    // make it unique to apply your CSS animations just to this exact popup
+    mainClass: 'mfp-zoom-in',
+    callbacks: {
+      beforeOpen: function() {
+        // just a hack that adds mfp-anim class to markup
+        this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+      }
+    },
+    closeOnContentClick: true,
+    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  });
 
 });
